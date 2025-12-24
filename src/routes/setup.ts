8b7a7f2c4ec62043b91470a39b0e84a6832def59
Matt Riley <matt@mattriley.me>
2025-12-24 12:50:45 +0000

    refactor: routes setup

--- src/routes/setup.ts
+++ src/routes/setup.ts
@@ -2,12 +2,12 @@ import Elysia from "elysia";
 import * as types from "../types";
 import { getDb } from "../db";
 
-const db = await getDb();
-
 export const setup = new Elysia({ name: "setup" })
   .model({
     quote: types.quote,
     quotes: types.quotes,
     intId: types.numeric_id,
   })
-  .decorate("db", db);
+  .derive(async () => ({
+    db: await getDb(),
+  }));
